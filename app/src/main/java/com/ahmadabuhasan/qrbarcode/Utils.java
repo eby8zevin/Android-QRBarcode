799e11987ee7bfe052340995b1448a0e88c72a2e
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-03-18 00:41:00 +0700

    added AdsDelay

--- app/src/main/java/com/ahmadabuhasan/qrbarcode/Utils.java
+++ app/src/main/java/com/ahmadabuhasan/qrbarcode/Utils.java
@@ -1,6 +1,7 @@
 package com.ahmadabuhasan.qrbarcode;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.Log;
 
 import com.google.android.gms.ads.AdListener;
@@ -9,18 +10,28 @@ import com.google.android.gms.ads.InterstitialAd;
 
 public class Utils {
 
+    public static InterstitialAd interstitialAd;
+
     public void interstitialAdsShow(Context context) {
-        final InterstitialAd interstitialAd = new InterstitialAd(context);
+        interstitialAd = new InterstitialAd(context);
         interstitialAd.setAdUnitId(context.getString(R.string.AdMob_Interstitial_Ads_ID));
         interstitialAd.loadAd(new AdRequest.Builder().build());
         interstitialAd.setAdListener(new AdListener() {
             @Override
             public void onAdLoaded() {
-                if (interstitialAd.isLoaded()) {
-                    interstitialAd.show();
-                    Log.d("interstitialAd", "Show");
-                }
+                AdsDelay();
+                Log.d("interstitialAd", "Show");
             }
         });
+    }
+
+    public void AdsDelay() {
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            if (interstitialAd == null || !interstitialAd.isLoaded()) {
+                return;
+            }
+            interstitialAd.show();
+        }, 30000);
     }
 }
